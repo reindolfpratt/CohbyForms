5c810e728116aabd6407869ad360fdf5411cd818
reindolfpratt <reindolfpratt@gmail.com>
2026-05-01 02:38:28 +0100

    fix: remove unused imports in survey follow-ups utils

--- apps/web/modules/survey/follow-ups/lib/utils.ts
+++ apps/web/modules/survey/follow-ups/lib/utils.ts
@@ -1,5 +1,4 @@
 import { Organization } from "@prisma/client";
-import { IS_FORMBRICKS_CLOUD, PROJECT_FEATURE_KEYS } from "@/lib/constants";
 
 export const getSurveyFollowUpsPermission = async (
   _billingPlan: Organization["billing"]["plan"]
